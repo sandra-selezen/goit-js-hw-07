e8c1a511262c11fa8dedcc3046720babf22ad616
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-02-13 12:21:01 +0200

    Adds english description of tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+/*
+
+Create a gallery with the ability to click on its items and view full size images in a modal window.
+
+Do this task in the 01-gallery.html and 01-gallery.js files. Break it down into several subtasks:
+
+1. Creating and rendering markup from the galleryItems data array and provided gallery item template.
+2. Implementing delegation to div.gallery and getting the url of a large image.
+3. Adding the script and styles of the modal window library basicLightbox. 
+Use the jsdelivrCDN service and add links to minified (.min) library files to your project.
+4. Opening a modal window by clicking on a gallery item. 
+To do this, check out the documentation and examples.
+5. Replacing the value of the src attribute of the <img> element in a modal window before opening. 
+Use the ready-made modal window markup with the image from the examples of the basicLightbox library.
+
+The link to the original image must be stored in the source data attribute on the <img> element and specified in the link's href.
+
+Please note that the image is wrapped in a link, which means that, when clicked, 
+the user will be redirected to another page by default. Disable this behavior by default.
+
+OPTIONAL
+
+Add modal window closing upon pressing the Escape key. 
+Make keyboard listening available only while the modal window is open. 
+In the basicLightbox library, there is a method to close the modal window programmatically.
+
+*/
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+/*
+
+Create the same gallery as in the first task, but using the SimpleLightbox library, 
+which will handle image clicks, modal opening and closing and image scrolling with the keyboard.
+
+1. Creating and rendering markup from the galleryItems data array and provided gallery element template. 
+Use the ready-made code from the first task.
+2. Adding the script and library styles using the cdnjs CDN service. 
+You need to add links to two files: simple-lightbox.min.js and simple-lightbox.min.css.
+3. Library initialization after gallery items are created and added to div.gallery. 
+To do this, read the SimpleLightbox documentation - first of all, the Usage and Markup sections.
+4. Look in the documentation for the Options section and add image caption display from the alt attribute. 
+Let the caption be at the bottom and appear 250 milliseconds after image opening.
+
+*/
